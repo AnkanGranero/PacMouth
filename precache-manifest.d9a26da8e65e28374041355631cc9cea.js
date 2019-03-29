3c07cca1df13c3bc1df29852ff5d4c56004ed114
Andreas <grannemannen@gmail.com>
2019-03-29 16:37:33 +0100

    Updates

---
+++ precache-manifest.d9a26da8e65e28374041355631cc9cea.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "45617641fee04c0023a47b59c0894a98",
+    "url": "/PacMouth/static/media/hotdog2.45617641.jpg"
+  },
+  {
+    "revision": "8f1ac24fa2c9827acb7e",
+    "url": "/PacMouth/static/css/main.3642cb3d.chunk.css"
+  },
+  {
+    "revision": "de8711c571bfc8466d93",
+    "url": "/PacMouth/static/js/runtime~main.ab861387.js"
+  },
+  {
+    "revision": "0ab5a191fdf6a248373e",
+    "url": "/PacMouth/static/js/2.bb8ae1fb.chunk.js"
+  },
+  {
+    "revision": "04b0451bf2fb72fdcd24fdbc0606ec70",
+    "url": "/PacMouth/static/media/hotdog1.04b0451b.jpg"
+  },
+  {
+    "revision": "8f1ac24fa2c9827acb7e",
+    "url": "/PacMouth/static/js/main.9b132b3d.chunk.js"
+  },
+  {
+    "revision": "18b39bc832f8733c68f4b348bad04f68",
+    "url": "/PacMouth/static/media/hotdog3.18b39bc8.jpg"
+  },
+  {
+    "revision": "0a8e5703989fda59fb051671b99f113d",
+    "url": "/PacMouth/static/media/hotdog4.0a8e5703.jpg"
+  },
+  {
+    "revision": "a9708de181209cf5a89d077f5cc35441",
+    "url": "/PacMouth/static/media/pacmouth.a9708de1.jpg"
+  },
+  {
+    "revision": "893f24a2fa209efb530a6fbb2486d2cd",
+    "url": "/PacMouth/static/media/pacmouth2.893f24a2.jpg"
+  },
+  {
+    "revision": "e894f0ea9b02e83bc1ada36f59904e7f",
+    "url": "/PacMouth/index.html"
+  }
+];
